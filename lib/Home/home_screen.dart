0b2b86b63749778234451f38b7889269591e8e75
ammarhussien <ammar.hussienmo97@gmail.com>
2023-05-02 11:57:29 +0300

    new

---
+++ lib/Home/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.menu),
+        ),
+      ),
+    );
+  }
+}
